0f4be77933529bf73f25bb3a643bfb148b565ede
joshzano <158782774+joshzano@users.noreply.github.com>
2024-05-09 09:42:01 -0700

    Update RPG.h

--- RPG.h
+++ RPG.h
@@ -1,1 +1,33 @@
+#ifndef RPG_H
+#define RPG_H
+
+using namespace std;
+
+const int SKILL_SIZE = 2;
+
+class RPG {
+    public:
+        //constructors
+        RPG();
+        RPG(string name, int health, int strength, int defense, string type)
+        void setSkills();
+        void printAction(string, RPG);
+        void updateHealth(int);
+        void attack(RPG *);
+        void useSkill(RPG *);
+        bool isAlive() const;
+        string getName() const;
+        int getHealth () const;
+        int getStrength () const;
+        int getDefense () const;
+
+    private;
+        string name;
+        int health;
+        int strength;
+        int defense;
+        string type; // warrior, mage, thief, archer
+        string skills[SKILL_SIZE];
+};
+#endif
 
